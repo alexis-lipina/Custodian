effe9b09d24dfc5cccc0424e8283e5d8b9af633c
unjer <mrs5980@g.rit.edu>
2017-10-28 11:49:08 -0400

    level and camera

---
+++ CustodianSimulator/Assets/Scripts/WallController.cs
@@ -0,0 +1,17 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class WallController : MonoBehaviour {
+
+    [SerializeField] private int wallToUse;
+
+    private Animator anim;
+
+    void Start()
+    {
+        anim = GetComponent<Animator>();
+
+        anim.SetFloat("Blend", wallToUse);
+    }
+}
