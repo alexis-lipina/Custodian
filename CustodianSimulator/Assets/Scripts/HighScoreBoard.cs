356c6091ae357a26cdf2c40de1bf285020cfa1bd
Davis Snider (RIT Student) <dls7470@ad.rit.edu>
2017-11-17 17:10:27 -0500

    stuff

---
+++ CustodianSimulator/Assets/Scripts/HighScoreBoard.cs
@@ -0,0 +1,43 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using System.Linq;
+
+public class HighScoreBoard : MonoBehaviour
+{
+    List<int> highScores;
+
+
+
+	// Use this for initialization
+	void Start ()
+    {
+        DontDestroyOnLoad(this.gameObject);
+
+        highScores = new List<int>();
+	}
+
+
+    public void AddScore(int score)
+    {
+        if (!highScores.Contains(score))
+        {
+            highScores.Add(score);
+        }
+    }
+
+
+    public int HighestAScore()
+    {
+        int lowest = int.MaxValue;
+        foreach(int tempScore in highScores)
+        {
+            if(tempScore < lowest)
+            {
+                lowest = tempScore;
+            }
+        }
+
+        return lowest;
+    }
+}
